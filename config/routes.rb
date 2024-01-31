39d465f54c21a7258ed37c8d06110551b74bab52
Joel Fregno <joelfregno@outlook.com>
2024-01-31 19:34:19 +0100

    rails task manager

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,15 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+  get "tasks", to: "tasks#tasks"
+  get "tasks/new", to: "tasks#new"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  get "tasks/:id", to: "tasks#show", as: :task
+  post "tasks", to: "tasks#create"
+  patch "tasks/:id", to: "tasks#update"
+
 
   # Defines the root path route ("/")
   # root "posts#index"
+
 end
